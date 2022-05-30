9a0179b9c5d9f79fd8561e64971ad3d2dc824423
juliankretz <juliankretz68@gmail.com>
2022-05-30 09:33:19 +0200

    4.3

--- 4.3/4.3/Program.cs
+++ 4.3/4.3/Program.cs
@@ -1,9 +1,15 @@
 ﻿String input;
+int length = 120;
+
 int amount = 0;
-char[] tab = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',};
+char[] tab = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 Console.WriteLine("Entrez un texte d'au moins 120 caractères");
 input = Console.ReadLine();
+if  (input length)
+{
+    Console.WriteLine("Veuillez entrez un texte d'au moins 120 caractères");
+}
 
 for (int i = 0; i < input.Length; i++)
 {
@@ -12,6 +18,6 @@ for (int i = 0; i < input.Length; i++)
         if (input[i] == amount)
        
         amount++;
-        Console.WriteLine("La lettre "+tab+" est apparue "+amount+" fois.");
+        Console.WriteLine("La lettre "+j+" est apparue "+amount+" fois.");
     }
 }
