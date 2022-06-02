f8bcd5bc66b0b439534d9e4beb137bcef16c867b
juliankretz <juliankretz68@gmail.com>
2022-06-02 12:52:56 +0200

    exo 4.4

--- 4.3/4.3/Program.cs
+++ 4.3/4.3/Program.cs
@@ -1,9 +1,9 @@
 ﻿String input;
 int inputLength = 10;
 int[] amount = new int[26];
-char[] tab = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char[] tab = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-Console.WriteLine("Entrez un texte d'au moins "+inputLength+" caractères");
+Console.WriteLine("Entrez un texte d'au moins " + inputLength + " caractères");
 
 do
 {
@@ -11,18 +11,18 @@ do
     input = input.Replace("é", "e").Replace("è", "e");
     if (input.Length < inputLength)
     {
-        Console.WriteLine("Veuillez entrez un texte d'au moins "+inputLength+" caractères");
+        Console.WriteLine("Veuillez entrez un texte d'au moins " + inputLength + " caractères");
     }
 } while (input.Length < inputLength);
 
-char [] inputChar = input.ToCharArray();
+char[] inputChar = input.ToCharArray();
 
 for (int i = 0; i < inputChar.Length; i++)
 {
     for (int j = 0; j < tab.Length; j++)
     {
         if (inputChar[i] == tab[j])
-        { 
+        {
             amount[j]++;
         }
     }
--- Exo 4.4/Exo 4.4/Program.cs
+++ Exo 4.4/Exo 4.4/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int[] tab = { 128, 64, 8, 512, 16, 32, 256 };
+
+for (int j = 0; j < tab.Length; j++)
+{
+    int a = j;
+    for (int i = j; i < tab.Length; i++)
+    {
+        if (tab[i] < tab[a])
+        {
+            a = i;
+        }
+    }
+
+    int p = tab[a];
+    tab[a] = tab[j];
+    tab[j] = p;
+  
+}
+Console.WriteLine();
