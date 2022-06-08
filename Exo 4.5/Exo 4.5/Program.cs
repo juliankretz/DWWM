33ba6ec64d77086d8536f9e39e965b9f6d403280
Julian <jkretz@gmail.com>
2022-06-08 16:53:38 +0200

    exo tab

---
+++ Exo 4.5/Exo 4.5/Program.cs
@@ -0,0 +1,27 @@
+﻿String input;
+bool palindrome = false;
+Console.WriteLine("Entrez une chaîne de caractères");
+input = Console.ReadLine();
+    
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 0; j < input.Length; j--)
+        {
+            
+        }
+}
+
+
+
+if (palindrome = true)
+{
+    Console.WriteLine("La chaîne de caractères est un palindrome");
+}
+else if (palindrome = false)
+{
+    Console.WriteLine("La chaîne de caractères n'est pas un palindrome");
+}
+else
+{
+    Console.WriteLine("La chaîne de caractères est vide");
+}
