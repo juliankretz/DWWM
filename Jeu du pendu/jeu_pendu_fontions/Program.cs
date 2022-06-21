f847e1129ccc2717abeacb575b98d3536173f2b9
Julian <jkretz@gmail.com>
2022-06-21 16:48:34 +0200

    jeu du pendu

--- Jeu du pendu/jeu_pendu_fontions/Program.cs
+++ Jeu du pendu/jeu_pendu_fontions/Program.cs
@@ -9,27 +9,82 @@ namespace jeu_pendu_fontions
             string motPendu;
             char debut, fin;
             int erreurs, longueur;
-            bool win = true;
+            bool win = false;
+            bool found = false;
 
-            Console.WriteLine("Joueur 1 : entrez un mot de plus 5 caractères");
-            motPendu = Console.ReadLine();
+            Console.WriteLine("JOUEUR 2 : Retournez-vous");
+            Console.WriteLine("JOUEUR 1 : Entrez un mot d'au moins 5 caractères");
+            
+            do
+            {
+                motPendu = Console.ReadLine();
+                if (motPendu.Length < 5)
+                {
+                    Console.WriteLine("Veuillez entrez un mot d'au moins 5 caractères");
+                }
+            } while (motPendu.Length < 5);
+            motPendu = motPendu.Replace("é", "e").Replace("è", "e");
             char[] motrecherche = motPendu.ToCharArray();
             for (int i = 1; i < motrecherche.Length - 1; i++)
             {
                 motrecherche[i] = '-';
             }
             Console.Clear();
-            foreach (char c in motrecherche)
-            {
-                Console.Write(c + " ");
-            }
 
-            int tries = 6;
+            int tries = 5;
+            Console.WriteLine("JOUEUR 2 : Devinez le mot choisit par le JOUEUR 1 en entrant un caractère à la fois");
+            Console.WriteLine("Vous avez droit à " + tries + " fautes en tout");
             do
             {
+                win = false;
+                foreach (char c in motrecherche)
+                {
+                    Console.Write(c + " ");
+                }
 
-            } while (tries >= 0);
+                Console.WriteLine("Vous avez encore droit à " + tries + " fautes");
 
+                char lettre = char.Parse(Console.ReadLine());
+                for (int i = 1; i < motPendu.Length - 1; i++)
+                {
+                    if (motPendu[i].Equals(lettre))
+                    {
+                        motrecherche[i] = lettre;
+                        win = true;                     
+                    }
+
+                }
+                found = true;
+                for (int i = 0; i < motrecherche.Length; i++)
+                {
+                    if (motrecherche[i] != motPendu[i])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (win == false)
+                {
+                    Console.WriteLine("La lettre " + lettre + " n'est pas présente dans le mot du JOUEUR 1");
+                    tries--;
+                }
+
+            } while (tries >= 0 && found == false);
+
+            if (tries > 0)
+            {
+                foreach (char c in motrecherche)
+                {
+                    Console.Write(c + " ");
+                }
+                Console.WriteLine("Bravo vous avez trouvé le mot en " + (6 - tries) + " essais infructueux");
+            }
+            else
+            {
+
+                Console.WriteLine("Vous avez perdu, le mot du JOUEUR 1 était : " + motPendu);
+            }
 
 
             Console.ReadKey();
