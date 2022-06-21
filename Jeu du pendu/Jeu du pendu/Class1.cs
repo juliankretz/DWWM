12a56aee444fbb1f331461d1def652a88388669c
Julian <jkretz@gmail.com>
2022-06-21 15:18:52 +0200

    pendu

---
+++ Jeu du pendu/Jeu du pendu/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu_du_pendu
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Jeu du pendu/jeu_pendu_fontions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace jeu_pendu_fontions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string motPendu;
+            char debut, fin;
+            int erreurs, longueur;
+            bool win = true;
+
+            Console.WriteLine("Joueur 1 : entrez un mot de plus 5 caractères");
+            motPendu = Console.ReadLine();
+            char[] motrecherche = motPendu.ToCharArray();
+            for (int i = 1; i < motrecherche.Length - 1; i++)
+            {
+                motrecherche[i] = '-';
+            }
+            Console.Clear();
+            foreach (char c in motrecherche)
+            {
+                Console.Write(c + " ");
+            }
+
+            int tries = 6;
+            do
+            {
+
+            } while (tries >= 0);
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+    }
+}
