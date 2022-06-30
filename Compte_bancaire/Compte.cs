2ab8137acbdc3cbfb4ac9e2296edd790447496a5
Julian <jkretz@gmail.com>
2022-06-30 09:30:35 +0200

    objet

---
+++ Compte_bancaire/Compte.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compte_bancaire
+{
+    public class Compte
+    {
+        // champs ou attributs 
+
+        private int decouvertAutorise;
+        private string nom;
+        private int numero;
+        private int solde;
+
+        // propriétés
+        public int DecouvertAutorise { get => decouvertAutorise; set => decouvertAutorise = value; }
+        public string Nom { get => nom; }
+        public int Numero { get => numero; }
+        public int Solde { get => solde; set => solde = value; }
+
+        // constructeur surchargé
+        public Compte(int _numero, string _nom, int _solde, int _decouvertAutorise)
+        {
+            this.numero = _numero;
+            this.nom = _nom;
+
+            this.Solde = _solde;
+            DecouvertAutorise = _decouvertAutorise;
+        }
+
+        // constructeur à vide
+        public Compte()
+        {
+            this.numero = 1;
+            this.nom = "Sans nom";
+            this.Solde = 0;
+            this.DecouvertAutorise = 0;
+        }
+
+        // méthodes
+        public void Crediter(int _montant)
+        {
+            this.Solde += _montant;
+        }
+        public bool Debiter(int _montant)
+        {
+            if (this.Solde - _montant >= this.decouvertAutorise)
+            {
+                this.Solde -= _montant;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string ToString()
+        {
+            string chaineCompte = "Le compte numéro " + this.Numero + " ayant comme propriétaire Mme/Mr " + this.Nom + " a pour solde " + this.Solde + " euros et un découvert autorisé de " + Math.Abs(this.DecouvertAutorise) + " euros.";
+            return chaineCompte;
+        }
+        public bool Transferer(int _montant)
+        {
+            
+        }
+
+    }
+}
---
+++ Compte_bancaire/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Compte_bancaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compte compte1 = new Compte(1002, "Tintin", 100, -300);
+            bool accord = compte1.Debiter(800);
+
+            if (accord == true)
+            {
+                Console.WriteLine("L'opération est autorisée, le nouveau solde du compte est : " + compte1.Solde);
+            }
+            else
+            {
+                Console.WriteLine("Opération impossible, dépassement du découvert autorisé");
+            }
+            Console.ReadLine();
+            Console.WriteLine(compte1);
+        }
+    }
+}
