63985283e56322fe7657004ee97975a53e5ab901
Julian <jkretz@gmail.com>
2022-09-27 16:06:52 +0200

    maj

---
+++ PHP/jalon.php
@@ -0,0 +1,15 @@
+<?php
+
+function Fibonacci($n, $nbp, $nbc, $pos)
+{
+    return "Les " + $n + " preimers nombres de la suite de Fibocanni sont :" . PHP_EOL;
+    return ($nbp) . PHP_EOL;
+    return ($nbc) . PHP_EOL;
+
+    if ($n > 2)
+    $nbs = $nbp + $nbc;
+
+}
+echo Fibonacci(10,0,1,0)
+
+
