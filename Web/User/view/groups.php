e148edcd6d4103edd0417bd2a8697808f1bc8a32
Julian <jkretz@gmail.com>
2022-10-03 16:20:18 +0200

    user

---
+++ Web/User/view/groups.php
@@ -0,0 +1,15 @@
+<h1>Groupes d'utilisateurs</h1>
+
+<form>
+    
+    <label>Identifiants</label>
+    <input type="number" name="group_id">
+
+    <br>
+
+    <label>Nom du groupe</label>
+    <input type="text" name="group_name"> 
+
+    <button type="submit">Valider</button>
+
+</form>
--- Web/User/www/index.php
+++ Web/User/www/index.php
@@ -8,8 +8,31 @@
 * - charset = jeu de caractères
 */
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=db_users;charset=utf8mb4';
+/* $dsn = 'mysql:host=localhost;port=3306;dbname=db_users;charset=utf8mb4';
 
-$connexion = new PDO($dsn, 'jkretz', '1234');
+$connexion = new PDO($dsn, 'jkretz', '1234'); */
 
+echo '<pre>' .var_export($_GET, true). '</pre>';
+
+// si le paramètre 'page' dans l'url n'est pas vide
+
+$page = $_GET['page'] ?? 'home';
+
+switch($page)
+{
+    case 'home';
+        //charger ici le fichier home.php
+        require '../view/home.php';
+    break;
+    case 'profile';
+        //charger ici le fichier profile.php
+        require '../view/profile.php';
+    break;
+    case 'groups';
+        require '../view/groups.php';
+        break;
+    default:
+        echo 'Erreur 404';
+    break;
+}
 
