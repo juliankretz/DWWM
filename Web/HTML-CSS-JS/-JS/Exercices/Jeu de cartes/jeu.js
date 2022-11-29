49ed2eb639f0a5c55e43d7279558759a1d36315d
Julian <jkretz@gmail.com>
2022-11-29 16:59:53 +0100

    js

--- Web/HTML-CSS-JS/-JS/Exercices/Jeu de cartes/jeu.js
+++ Web/HTML-CSS-JS/-JS/Exercices/Jeu de cartes/jeu.js
@@ -1,3 +1,47 @@
+function createCell(myValue, myRow)
+{
+    let myCell= myRow.insertCell();
+    myCell.textContent = myValue;
+    
+}
+
+
+function createTitleCell(myValue, myRow)
+{
+    let myTitleCell = document.createElement("th");
+    myTitleCell.textContent = myValue;
+    myRow.append(myTitleCell);
+
+}
+
+function tableauJour(_data) 
+{
+    let myTab = document.getElementById("perso");
+
+    let myTHead = myTab.createTHead();
+    
+    let titleRow = myTHead.insertRow();
+
+    for (const key in _data[0])
+    {
+        createTitleCell(key, titleRow);    
+    }
+
+    let myBody = myTab.createTBody();
+    
+    for (let i = 0; i < _data.length; i++)
+    {
+        let myRow = myBody.insertRow();
+        
+        for (var key in _data[i])
+        {
+            createCell(_data[i][key], myRow);
+        }
+    }
+
+}
+
+
 var monXHR = new XMLHttpRequest();
 monXHR.open("GET", "./cardgame.json", true);
 
@@ -15,15 +59,12 @@ monXHR.onload= function()
     else
     {
         let data = monXHR.response;
+
+      tableauJour(data);
+
         console.log(data);
     }
 } 
 
 
 
-function tableauJour(data) 
-{
-
-
-
-}
