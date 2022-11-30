b9fd7221a47952b3558a2fcfad4557deb4d198d4
Julian <jkretz@gmail.com>
2022-11-30 16:56:32 +0100

    js

--- Web/HTML-CSS-JS/-JS/Exercices/Jeu de cartes/jeu.js
+++ Web/HTML-CSS-JS/-JS/Exercices/Jeu de cartes/jeu.js
@@ -41,6 +41,7 @@ function tableauJour(_data)
 
 }
 
+var tabData = [];
 
 var monXHR = new XMLHttpRequest();
 monXHR.open("GET", "./cardgame.json", true);
@@ -60,11 +61,96 @@ monXHR.onload= function()
     {
         let data = monXHR.response;
 
-      tableauJour(data);
+        tabData = data;
+        
+        tableauJour(data);
 
         console.log(data);
     }
 } 
 
+function DisplayHighestAtk(data)
+{
+    let maxAtk = data[0].attack;
 
+    let indice = 0;
+    
+    for (let i = 0; i < data.length; i++)
+    {
+        if (data[i].attack > maxAtk)
+        {
+            maxAtk = data[i].attack;
+            indice = i;
+        } 
+    }
+    return indice;
+}
 
+function DisplayLowestAtk(data)
+{
+    let minAtk = data[0].attack
+
+    let indice = 0;
+
+    for (let i = 0; i < data.lenght; i++)
+    {
+        if (data[i].attack < minAtk)
+        {
+            minAtk = data[i].attack
+            indice = i;
+        }
+    }
+    return indice;
+}
+
+document.getElementById("btnAtk").addEventListener("click", function()
+{
+    let indiceMax = DisplayHighestAtk(tabData);
+    document.getElementById("summarizeHigh").innerHTML = "<p>" + tabData[indiceMax].name + " has the highest attack : " + tabData[indiceMax].attack + "</p>"
+
+    let indiceMin = DisplayLowestAtk(tabData);
+    document.getElementById("summarizeLow").innerHTML = "<p>" + tabData[indiceMin].name + " has the lowest attack : " + tabData[indiceMin].attack + "</p>"
+})
+
+function DisplayHighestArmor(data)
+{
+    let maxArmor = data[0].armor;
+
+    let indice = 0;
+    
+    for (let i = 0; i < data.length; i++)
+    {
+        if (data[i].armor > maxArmor)
+        {
+            maxArmor = data[i].armor;
+            indice = i;
+        } 
+    }
+    return indice;
+}
+
+function DisplayLowestArmor(data)
+{
+    let minArmor = data[0].armor;
+
+    let indice = 0;
+    
+    for (let i = 0; i < data.length; i++)
+    {
+        if (data[i].armor < minArmor)
+        {
+            minArmor = data[i].armor;
+            indice = i;
+        }
+    }
+    return indice;
+}
+
+document.getElementById("btnArmor").addEventListener("click", function()
+{
+    let indiceMax = DisplayHighestAtk(tabData);
+    document.getElementById("summarizeHigh").innerHTML = "<p>" + tabData[indiceMax].name + " has the highest armor : " + tabData[indiceMax].armor + "</p>"
+    
+    let indiceMin = DisplayLowestAtk(tabData);
+    document.getElementById("summarizeLow").innerHTML = "<p>" + tabData[indiceMin].name + " has the lowest armor : " + tabData[indiceMin].attack + "</p>"
+})
