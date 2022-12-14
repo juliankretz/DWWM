64076fa2399add82e2201256b67f2c1e9dbab8af
Julian <jkretz@gmail.com>
2022-12-14 16:50:09 +0100

    js

--- Web/HTML-CSS-JS/-JS/Exercices/Employees/employees.js
+++ Web/HTML-CSS-JS/-JS/Exercices/Employees/employees.js
@@ -15,11 +15,54 @@
 let data = [];
 fetch("employees.json")
 .then(response => { return response.json() })
-.then(response => { genererListe(response); data = response; })
 .catch(erreur =>  { console.error(erreur) })
 
+console.log(data);
+
+function createCell(myValue, myRow)
+{
+    let myCell= myRow.insertCell();
+    myCell.textContent = myValue;
+}
+
+function createTitleCell(myValue, myRow)
+{
+    let myTitleCell = document.createElement("th");
+    myTitleCell.textContent = myValue;
+    myRow.append(myTitleCell);
+}
+
+function tableauJour(_data) 
+{
+    let myTab = document.getElementById("tabList");
+    let myTHead = myTab.createTHead();
+    let titleRow = myTHead.insertRow();
+
+    for (const key in _data[0])
+    {
+        createTitleCell(key, titleRow);    
+    }
+
+    let myBody = myTab.createTBody();
+    
+    for (let i = 0; i < _data.length; i++)
+    {
+        let myRow = myBody.insertRow();
+        
+        for (var key in _data[i])
+        {
+            createCell(_data[i][key], myRow);
+        }
+    }
+
+}
+
+function monthlySalary(data)
+{
+    let yearSalary = data[0].employee_salary
+}
 
 function createEmail(data)
 {
-
+    let fullName = data[0].employee_name
 }
